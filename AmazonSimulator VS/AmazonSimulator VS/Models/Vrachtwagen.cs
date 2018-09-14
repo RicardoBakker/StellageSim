71fc352826ac60c6623fdec54a010c01ff234a0b
Fabiandw <32310554+Fabiandw@users.noreply.github.com>
2018-09-14 11:27:34 +0200

    09-14-2018 V1.1
    42 files. Coincidence? I think not.

---
+++ AmazonSimulator VS/AmazonSimulator VS/Models/Vrachtwagen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models 
+{
+    public class Vrachtwagen : C3Dmodel
+    {
+        public Vrachtwagen(double x, double y, double z, double rotationX, double rotationY, double rotationZ) : base(x, y, z, rotationX, rotationY, rotationZ)
+        {
+            this.type = "vrachtwagen";
+            this.needsUpdate = true;
+        }
+    }
+}
