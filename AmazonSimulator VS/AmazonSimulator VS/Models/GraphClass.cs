655a612a62ca8dc75a16044d967e8570043f3dea
Fabiandw <fabian.de.wilde94@gmail.com>
2018-10-02 19:38:32 +0200

    GraphClass

--- AmazonSimulator VS/AmazonSimulator VS/Models/GraphClass.cs
+++ AmazonSimulator VS/AmazonSimulator VS/Models/GraphClass.cs
@@ -1,11 +1,162 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Graph
-{
-}
+    public class GraphNode<T>
+    {
+        T value;
+        List<GraphNode<T>> neighbors;
+
+        public GraphNode(T value)
+        {
+            this.value = value;
+            neighbors = new List<GraphNode<T>>();
+        }
+        public T Value
+        {
+            get { return value; }
+
+        }
+
+        public IList<GraphNode<T>> Neighbors
+        {
+            get { return neighbors.AsReadOnly(); }
+        }
+        public bool AddNeighbor(GraphNode<T> neighbor)
+        {
+            if (neighbors.Contains(neighbor))
+            {
+                return false;
+            }
+            else
+            {
+                neighbors.Add(neighbor);
+                return true;
+            }
+
+        }
+        public bool RemoveNeighbor(GraphNode<T> neighbor)
+        {
+            return neighbors.Remove(neighbor);
+        }
+        public bool RemoveAllNeighbors()
+        {
+            for (int i = neighbors.Count - 1; i <= 0; i--)
+            {
+
+            }
+            return true;
+        }
+
+    }
+
+    public class Graph<T>
+    {
+        List<GraphNode<T>> nodes = new List<GraphNode<T>>();
+        public Graph()
+        {
+
+        }
+        public int Count
+        {
+            get { return nodes.Count; }
+        }
+        public IList<GraphNode<T>> Nodes
+        {
+            get { return nodes.AsReadOnly(); }
+        }
+        public void Clear()
+        {
+            foreach (GraphNode<T> node in nodes)
+            {
+                node.RemoveAllNeighbors();
+            }
+            for (int i= nodes.Count -1; i>= 0; i--)
+            {
+                nodes.RemoveAt(i);
+            }
+        }
+        public bool AddNode (T value)
+        {
+            if (Find(value) != null)
+                {
+                return false;
+            }
+            else {
+                nodes.Add(new GraphNode<T>(value));
+                return true;
+            }
+            
+        }
+        public bool AddEdge (T value1, T value2)
+        {
+            GraphNode<T> node1 = Find(value1);
+            GraphNode<T> node2 = Find(value2);
+            if (node1 == null || node2 == null)
+            {
+                return false;
+            }
+            else if (node1.Neighbors.Contains(node2))
+            {
+                return false;
+            }
+            else
+            {
+                node1.AddNeighbor(node2);
+                node2.AddNeighbor(node1);
+                return true;
+            }
+
+        }
+        public bool RemoveEdge(T value1, T value2)
+        {
+            GraphNode<T> node1 = Find(value1);
+            GraphNode<T> node2 = Find(value2);
+            if (node1 == null || node2 == null)
+            {
+                return false;
+            }
+            else if (!node1.Neighbors.Contains(node2))
+            {
+                return false;
+            }
+            else
+            {
+                node1.RemoveNeighbor(node2);
+                node2.RemoveNeighbor(node1);
+                return true;
+            }
+
+        }
+        public GraphNode<T> Find(T value)
+        {
+            foreach (GraphNode<T> node in nodes)
+            {
+                if (node.Value.Equals(value))
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+            {
+                builder.Append(nodes[i].ToString());
+                if (i<Count-1)
+                {
+                    builder.Append(",");
+                }
+
+            }
+            return base.ToString();
+        }
+
+    }
 }
